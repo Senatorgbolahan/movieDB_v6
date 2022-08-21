c943117533e1873fdd81f3b5dacea2ac6f1907d3
Senatorgbolahan <efunkoyaomogbolahan@gmail.com>
2022-08-21 23:40:16 +0100

    movieDB api search with react router v6 & context api

---
+++ src/useFetch.js
@@ -0,0 +1,44 @@
+
+import React, {useEffect, useState} from 'react'
+
+// make sure to use https
+const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`
+
+
+const useFetch = (urlParams) => {
+    const [isLoading, setIsLoading] = useState(true)
+    const [error, setError] = useState({ show: false, msg: ''});
+    const [data, setData] = useState(null)
+
+    const fetchMovies = async (url) => {
+        setIsLoading(true)
+        try {
+         const response = await fetch(url)
+         const data = await response.json()
+     
+         if (data.Response === 'True') {
+            setData(data.Search)
+           setError({show: false, msg: ""})
+         }
+         else{
+           setError({show: true, msg: data.Error})
+         }
+         setIsLoading(false)
+        } catch (error) {
+          console.log(error);
+        }
+     
+       }
+     
+       useEffect(() => {
+         fetchMovies(`${API_ENDPOINT}&s=${urlParams}`)
+       }, [urlParams])
+
+
+  return {
+        isLoading, error, data
+  }
+}
+
+
+export default useFetch;
